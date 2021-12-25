7c664416526f368119ed446686794d3621de2759
TinyEnd <rowbert121@outlook.com>
2021-12-25 22:51:26 +0800

    OpenGL : mesh rfractor
    Signed-off-by: TinyEnd <rowbert121@outlook.com>

--- src/backend/core/Mesh.h
+++ src/backend/core/Mesh.h
@@ -9,38 +9,36 @@
 #include <assimp/mesh.h>
 
 namespace core {
-struct Vertex {
-    glm::vec3 position;
-    glm::vec3 tangent;
-    glm::vec3 binormal;
-    glm::vec3 normal;
-    glm::vec3 color;
-    glm::vec2 uv;
-};
+    struct Vertex {
+        glm::vec3 position;
+        glm::vec3 tangent;
+        glm::vec3 binormal;
+        glm::vec3 normal;
+        glm::vec3 color;
+        glm::vec2 uv;
+    };
 
-struct Texture {
-    uint8_t * texture_data;
-    const char* texture_path;
-    uint32_t  width;
-    uint32_t  height;
-    uint8_t  one_pixel_size; ///RGBA8
-    uint32_t total_size;
-};
+    struct Texture {
+        uint8_t * texture_data;
+        const char* texture_path;
+        uint32_t  width;
+        uint32_t  height;
+        uint8_t  one_pixel_size; ///RGBA8
+        uint32_t total_size;
+    };
 
-
-
-class Mesh {
-public:
-    Mesh() =default;
-    virtual ~Mesh() =default;
-
-    bool import(const char* path);
-
-protected:
-    std::vector<Vertex> vertices;  //顶点数据
-    std::vector<uint32_t> indices; //顶点索引
-    std::vector<Texture> textures;
-};
+    //class Mesh {
+    //public:
+    //    Mesh() =default;
+    //    virtual ~Mesh() =default;
+    //
+    //    bool import(const char* path);
+    //
+    //protected:
+    //    std::vector<Vertex> vertices;  //顶点数据
+    //    std::vector<uint32_t> indices; //顶点索引
+    //    std::vector<Texture> textures;
+    //};
 
 
     bool import(const char* path);
